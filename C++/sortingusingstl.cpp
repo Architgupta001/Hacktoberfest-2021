ebd0cb333ead6ba5d247f9047ae97f0454c23c69
Afzal Khan <afzalkhan.lma@gmail.com>
2021-10-01 20:05:49 +0530

    Stl Code

--- C++/sortingusingstl.cpp
+++ C++/sortingusingstl.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main()
 {
-int arr[] = {1, 3, 4, 2, 5 };
+int arr[] = {1, 3, 4, 2, 6};
 	int n = sizeof(arr)/sizeof(arr[0]);
     sort(arr,arr+n);
     for(int i =0;i<n;i++)
