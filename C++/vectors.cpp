fadbf9cc0fe4db3f12959d6a11aa65c373f8aa93
Rohan kumar <48005711+codebloded@users.noreply.github.com>
2021-10-05 00:31:23 +0530

    Create vectors.cpp

---
+++ C++/vectors.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int *arr = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j <= n; j++)
+        {
+            for (int k = i; k < j; k++)
+            {
+                cout << arr[k] << "\t";
+            }
+            cout << endl;
+        }
+    }
+}
+//Time complexity O(n);
