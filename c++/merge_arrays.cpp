6a1516aded2b980310d77b0d894442342b6aeed7
Rohan kumar <48005711+codebloded@users.noreply.github.com>
2021-10-05 00:30:16 +0530

    Update merge_arrays.cpp

--- c++/merge_arrays.cpp
+++ c++/merge_arrays.cpp
@@ -1,34 +1,25 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-
-int get_min(int *arr, int size)
-{
-    int result = arr[0];
-    for (int i = 0; i <= size; i++)
-        result = min(result, arr[i]);
-    return result;
-}
-int get_max(int *arr, int size)
-{
-    int result = arr[0];
-    for (int i = 0; i <= size; i++)
-        result = max(result, arr[i]);
-    return result;
-}
 
 int main()
 {
     int n;
     cin >> n;
-    int *arr = new int(n);
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    int pivot = 0;
+    int j = -1;
     for (int i = 0; i < n; i++)
     {
-        cin >> arr[i];
+        if (arr[i] < pivot)
+        {
+            j++;
+            swap(arr[j], arr[i]);
+        }
     }
-    cout << "Minimun " << get_min(arr, n) << "\n";
-    cout << "Maximun " << get_max(arr, n) << "\n";
-
-    delete (arr);
-
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
     return 0;
 }
