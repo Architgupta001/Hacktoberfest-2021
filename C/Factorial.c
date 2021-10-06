a8c258f16209797ac0ff1a023327ba87b2b69229
Razor-123 <84318919+Razor-123@users.noreply.github.com>
2021-10-06 11:56:48 +0530

    Added description in Factorial.c
    Added some description line in in Factorial.c

--- C/Factorial.c
+++ C/Factorial.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
   
-int factorial(unsigned int n){
+
+// Function to calculate factorial
+
+int factorial(int n){
     if (n == 0)
-        return 1;
+        return 1;  //  base case of function
+  
     return n * factorial(n - 1);
 }
 
 int main(){
-    int num = 5;
-    printf("Factorial of %d is %d", num, factorial(num));
+    int num = 5; // number whose factorial is to be found
+  
+    printf("Factorial of %d is %d", num, factorial(num)); // printing the factorial of number
+  
     return 0;
 }
