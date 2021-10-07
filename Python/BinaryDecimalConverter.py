379c87bd433d1f365c640cc6301354e4cb0e579e
Sarthak Ray <sarthakray2002@gmail.com>
2021-10-07 21:51:34 +0530

    Binary to Decimal

---
+++ Python/BinaryDecimalConverter.py
@@ -0,0 +1,32 @@
+decimal_input = int(input())
+def dec_to_bin(decimalnumber):
+    binary = ""
+    while(decimalnumber!=0):
+        remainder = decimalnumber%2
+        binary = str(remainder) + binary
+        decimalnumber = decimalnumber//2
+    
+    print(binary)
+
+dec_to_bin(decimal_input)  
+
+
+
+
+binary_input = input()
+def bin_to_dec(binarynumber):
+    binarynumber = binarynumber[::-1]
+    i = 0
+    decimal = 0
+    for digit in binarynumber:
+        decimal += int(digit)*(2**i)
+        i += 1
+
+    print(decimal)
+
+bin_to_dec(binary_input)
+
+
+
+
+    
