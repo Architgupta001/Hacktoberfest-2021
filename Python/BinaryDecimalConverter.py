ed570b1f779925dbdc2dd428a2386caa69bf514e
Sarthak Ray <sarthakray2002@gmail.com>
2021-10-08 13:17:47 +0530

    Update BinaryDecimalConverter.py

--- Python/BinaryDecimalConverter.py
+++ Python/BinaryDecimalConverter.py
@@ -1,9 +1,11 @@
-decimal_input = int(input("Enter decimal number: "))
+decimal_input = int(input("Enter decimal number: ")) #Taking Decimal number as input.
+
+#Function to convert decimal number to binary
 def dec_to_bin(decimalnumber):
     binary = ""
     while(decimalnumber!=0):
         remainder = decimalnumber%2
-        binary = str(remainder) + binary
+        binary = str(remainder) + binary #Concatenating successive binary digits
         decimalnumber = decimalnumber//2
     
     print(binary)
@@ -13,13 +15,15 @@ dec_to_bin(decimal_input)
 
 
 
-binary_input = input("Enter binary number: ")
+binary_input = input("Enter binary number: ") #Taking Binary number as input.
+
+#Function to convert binary to decimal number
 def bin_to_dec(binarynumber):
-    binarynumber = binarynumber[::-1]
+    binarynumber = binarynumber[::-1] #Reversing the input to string
     i = 0
     decimal = 0
     for digit in binarynumber:
-        decimal += int(digit)*(2**i)
+        decimal += int(digit)*(2**i) #Multiplying successive binary digits with increasing powers of 2.
         i += 1
 
     print(decimal)
