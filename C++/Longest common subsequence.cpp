715dfb4dd9f49aabea8c3b34b6bc0a10d1f34701
Satyam mahajan <54473091+satyam-mahajan@users.noreply.github.com>
2021-10-08 12:37:40 +0530

    Update Longest common subsequence.cpp

--- C++/Longest common subsequence.cpp
+++ C++/Longest common subsequence.cpp
@@ -1,67 +1,32 @@
-// This code prints the longest common subsequence between two strings using dp
-#include<bits/stdc++.h>
-using namespace std;
-#define fast              ios::sync_with_stdio(false);cin.tie(0);
-
-string printlongestsequence(string a, string b)
+class Solution
 {
-    int n=a.length(), m=b.length();
-    int t[n+1][m+1];
+public:
 
-    // initialising matrix
-    for(int i=0;i<=n;i++)
+int longestCommonSubsequence(string text1, string text2)
+{
+    int n = text1.size();
+    int m = text2.size();
+
+    int dp[n + 1][m + 1];
+
+    for (int i = 0; i <= n; i++)
     {
-        for(int j=0;j<=m;j++)
+        for (int j = 0; j <= m; j++)
         {
-            if(i==0 || j==0)
+            if (i == 0 or j == 0)
             {
-                t[i][j]=0;
+                dp[i][j] = 0;
+            }
+            else if (text1[i - 1] == text2[j - 1])
+            {
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+            }
+            else
+            {
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
             }
         }
     }
-
-    //filling matrix
-    for(int i=1;i<=n;i++)
-    {
-        for(int j=1;j<=m;j++)
-        {
-            if(a[i-1]==b[j-1])
-                t[i][j]= 1+ t[i-1][j-1];
-            else
-                t[i][j]= max(t[i-1][j], t[i][j-1]);
-        }
-    }
-
-    string answer="";
-    int i=n,j=m;
-
-    while(i>0 && j>0)
-    {
-        if(a[i-1]==b[j-1])
-        {
-            answer.push_back(a[i-1]);
-            i--, j--;
-        }
-        else{
-            if(t[i-1][j] > t[i][j-1])
-                i--;
-            else
-                j--;
-        }
-    }
-
-    reverse(answer.begin(), answer.end());
-    return answer;
-
-
+    return dp[n][m];
 }
-
-int main()
-{
-    fast
-    string a="AGGTAB";
-    string b="GXTXAYB";
-
-    cout<< printlongestsequence(a,b);
-    return 0;
-}
+};
