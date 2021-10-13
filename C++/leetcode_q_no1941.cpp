6347723ea4a91d1fa3120ff15ca6217d324ee758
Hitesh <76858968+hitesh1906@users.noreply.github.com>
2021-10-13 15:26:29 +0530

    Update leetcode_q_no1941.cpp
    Added some comment lines for an explanation.

--- C++/leetcode_q_no1941.cpp
+++ C++/leetcode_q_no1941.cpp
@@ -1,23 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// max_element is a function to calculate the maximum value in array
+
 class Solution {
 public:
     bool areOccurrencesEqual(string s) {
         int character;
-        int arr[26]={ };
+        int arr[26]={ };  //initializing all the element with zero
         for(int i=0;i<s.length();i++)
         {
-            character=s[i];
-            arr[character-97]++;
+            character=s[i];  // character is a variable which stores a value of string
+            arr[character-97]++;  // increase the value of array by one
         }
 
         for(int i=0;i<26;i++)
         {
-           if(arr[i]==*max_element(arr, arr + 26) || arr[i]==0)
+           if(arr[i]==*max_element(arr, arr + 26) || arr[i]==0) 
                continue;
 
-           if(arr[i]<*max_element(arr, arr + 26)|| arr[i]>*max_element(arr, arr + 26))
+           if(arr[i]<*max_element(arr, arr + 26)|| arr[i]>*max_element(arr, arr + 26))   
                 return false;
         }
         return true;
